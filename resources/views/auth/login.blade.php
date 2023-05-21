70e731d91570118d962e29fe4b5e3d34e1ee29c4
Aclaputra <muhamadacla22@gmail.com>
2023-05-21 19:01:32 +0700

    intiial commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,22 +37,46 @@
                     <img class="mx-auto" src="{{asset('/img/shiba.png')}}" alt="">
                     <h1 class="text-center text-light fw-bold">Log-in</h1>
                 </div>
-                <form class="card container p-4">
-
+                <form method="POST" action="{{route('login')}}" class="card container p-4">
+                    @csrf
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Username / Email</label>
-                        <input type="email" autofocus placeholder="shibasukses@gmail.com" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <label for="exampleInputEmail1" class="form-label">Email</label>
+                        <input name="email" id="email" type="email" autofocus placeholder="shibasukses@gmail.com" class="form-control @error('email') is-invalid @enderror" aria-describedby="emailHelp">
                         <div id="emailHelp" class="form-text text-dark">We'll never share your email with anyone else.</div>
+
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" placeholder="***********" class="form-control" id="exampleInputPassword1">
+                        <input type="password" placeholder="***********" class="form-control @error('password') is-invalid @enderror" name="password" id="password" required autocomplete="current-password">
+
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                        @enderror
                     </div>
                     <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                        <label class="form-check-label" for="remember">
+                            {{ __('Remember Me') }}
+                        </label>
                     </div>
-                    <button type="submit" class="btn btn-success mb-4">Submit</button>
+
+                    <button type="submit" class="btn btn-success">
+                        {{ __('Login') }}
+                    </button>
+
+                    @if (Route::has('password.request'))
+                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                            {{ __('Forgot Your Password?') }}
+                        </a>
+                    @endif
                 </form>
             </div>
         </div>
